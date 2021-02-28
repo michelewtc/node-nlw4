a4315487187d3c44045c410aa1b22af813514f68
michelewtc <michelechan1996@gmail.com>
2021-02-28 19:22:12 -0300

    created SendMailServices

--- api/src/controllers/SendMailController.ts
+++ api/src/controllers/SendMailController.ts
@@ -3,6 +3,7 @@ import { getCustomRepository } from "typeorm";
 import { SurveysRepository } from "../repositories/SurveysRepository";
 import { SurveysUsersRepository } from "../repositories/SurveysUsersRepository";
 import { UsersRepository } from "../repositories/UsersRepository";
+import SendMailService from "../services/SendMailService";
 
 
 class SendEmailController {
@@ -22,9 +23,9 @@ class SendEmailController {
             });
         }
 
-        const surveyAlreadyExists = await surveysRepository.findOne({ id: survey_id });
+        const survey = await surveysRepository.findOne({ id: survey_id });
 
-        if(!surveyAlreadyExists) {
+        if(!survey) {
             return response.status(400).json({
                 error: "Survey does not exists!",
             });
@@ -38,6 +39,8 @@ class SendEmailController {
         await surveysUsersRepository.save(surveyUser);
 
         //Send email to User
+        await SendMailService.execute(email, survey.title, survey.description);
+        
         return response.json(surveyUser);
     }
 }
