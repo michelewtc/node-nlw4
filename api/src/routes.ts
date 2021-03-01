6fdbe637587d4c9d55f33254282e9c49aded8acc
michelewtc <michelechan1996@gmail.com>
2021-03-01 01:06:58 -0300

    created AnswerController

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -2,14 +2,16 @@ import { Router } from 'express';
 import { SendEmailController } from './controllers/SendMailController';
 import { SurveysController } from './controllers/SurveysController';
 import { UserController } from './controllers/UserController';
+import { AnswerController } from './controllers/AnswerController';
 
 const router = Router();
-
 
 const userController = new UserController();
 const surveysController = new SurveysController();
 
 const sendMailController = new SendEmailController();
+
+const answerController = new AnswerController();
 
 router.post("/users", userController.create);
 
@@ -17,5 +19,7 @@ router.post("/surveys", surveysController.create);
 router.get("/surveys", surveysController.show);
 
 router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
 
 export { router };
